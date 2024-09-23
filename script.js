9b9f6e0aac8057152d3f5a4b499097b502400c66
Oleg <oxegay@gmail.com>
2024-09-23 16:57:52 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const menu = document.querySelector('.menu')
 burger.onclick = () => {
     menu.classList.toggle('open')
 }
+
+
+const modalBtn = document.querySelector('#modalBtn')
+const modal = document.querySelector('.modal')
+const closeBtn = document.querySelector('#closeBtn')
+
+modalBtn.onclick = () => {
+    modal.classList.add('open-modal')
+}
+
+closeBtn.onclick = () => {
+    modal.classList.remove('open-modal')
+}
+
+modal.onclick = (e) => {
+    // modal.classList.remove('open-modal')
+    console.log(e.target);
+    
+    if(e.target.closest('.modal__box')) {
+        modal.classList.remove('open-model')
+    }
+}
