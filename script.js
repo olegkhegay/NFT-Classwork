507715f5158566da48bb27a2a385095cb790a8c7
Oleg <oxegay@gmail.com>
2024-09-29 13:26:17 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ modal.onclick = (e) => {
     // modal.classList.remove('open-modal')
     console.log(e.target);
     
-    if(!e.target.closest('.modal__box')) {
+    if(e.target.closest('.modal__box')) {
         modal.classList.remove('open-model')
     }
 }
@@ -345,3 +345,41 @@ topCreators.forEach(card => {
         </div>
     `
 })
+
+document.getElementById('telegramForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const token = '7602838164:AAHVksccv2bxQVnqbbIx8SsTM6MLWHs74hM';  // Замените на ваш токен
+    const chat_id = '-4552747358';  // Замените на ваш chat_id
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    // Формируем сообщение
+    const message = `Name: ${name}\nPhone: ${phone}`;
+
+    const data = {
+      chat_id: chat_id,
+      text: message
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.ok) {
+        alert("Message sent!");
+      } else {
+        alert("Error sending message.");
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert("Error sending message.");
+    });
+  });
